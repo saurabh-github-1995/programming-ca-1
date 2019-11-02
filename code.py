6c7fed4a105217235925e6b1d5217044b17968dd
saurabh devade <saurabh.devade21@gmail.com>
2019-11-02 23:02:03 +0000

    1.function for sorting implemented

--- code.py
+++ code.py
@@ -1,2 +1,16 @@
 #initializing empty student list which will containe number of dictionaries of student data
 studentsList=[{"name":"Saurabh","number":11111111,"grade":"56"}]
+
+
+def sortList():
+    length=len(studentsList)
+    for i in range(length-1):
+        if(studentsList[i+1]["number"]<studentsList[i]["number"]):
+           studentsList[i]["number"],studentsList[i+1]["number"]=studentsList[i+1]["number"],studentsList[i]["number"]
+           sortList()
+
+#function for mregisterenig stuentd one at a time
+def registerStudenrs(student):
+    
+    #studentsList.append(student)
+    sortList2(student)
