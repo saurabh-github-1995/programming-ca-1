4fd5c6411d055a132e454e389f7fd98436ecb764
saurabh devade <saurabh.devade21@gmail.com>
2019-11-03 23:25:19 +0000

    final working code

--- binary.py
+++ binary.py
@@ -1,38 +1,49 @@
-"""studentsData=[{"name":"Saurabh","number":1,"grade":"56"},
-         {"name":"Saurabh","number":2,"grade":"56"},
-         {"name":"Saurabh","number":3,"grade":"56"},
-         {"name":"Saurabh","number":4,"grade":"56"},
-         {"name":"Saurabh","number":6,"grade":"56"}]"""
-
 studentsData=[]
-n=11111111
-for i in range(10000000):
+"""n=11111111
+for i in range(1000):
     #print(i)
     n=n+i
     student={"name":"Saurabh","number": i,"grade":"56"}
-    studentsData.append(student)
-
+    studentsData.append(student)"""
+studentObjGlobal={}
 def insertDataAtSpecificPostion(targetStudentNo,startIndex,lastIndex):
-    print(startIndex,lastIndex)
-    if(startIndex>lastIndex):
-        studentsData.insert(startIndex,studentObj)
-        #print(startIndex)
-        return startIndex
+    if(len(studentsData)==0):
+        studentsData.append(studentObjGlobal)
     else:
-        midpointIndex=startIndex+(lastIndex-startIndex)//2
-        if(studentsData[midpointIndex]["number"]==targetStudentNo):
-            print("number already exist")
-            return midpointIndex
+        if(targetStudentNo>studentsData[-1]["number"]):
+            studentsData.append(studentObjGlobal)
         else:
-            if(targetStudentNo<studentsData[midpointIndex]["number"]):
-                insertDataAtSpecificPostion(targetStudentNo,startIndex,midpointIndex-1)
+
+            if(startIndex>lastIndex):
+                studentsData.insert(startIndex,studentObjGlobal)
+                #print(startIndex)
+                return startIndex
             else:
-                insertDataAtSpecificPostion(targetStudentNo,midpointIndex+1,lastIndex)
-                
+                midpointIndex=startIndex+(lastIndex-startIndex)//2
+                if(studentsData[midpointIndex]["number"]==targetStudentNo):
+                    print("Student record with number already exist")
+                    return midpointIndex
+                else:
+                    if(targetStudentNo<studentsData[midpointIndex]["number"]):
+                        insertDataAtSpecificPostion(targetStudentNo,startIndex,midpointIndex-1)
+                    else:
+                        insertDataAtSpecificPostion(targetStudentNo,midpointIndex+1,lastIndex)
+
 
 def addStudentToList(studentObj):
+    global studentObjGlobal 
+    studentObjGlobal=studentObj
     insertDataAtSpecificPostion(studentObj['number'],0,len(studentsData))
     
     
-    
+def retriveStudentData():
+    if(len(studentsData)!=0):
+        loweststudent=studentsData[0]
+        del studentsData[0]
+        return loweststudent
+    else:
+        return ""
+
+def removeStudentData():
+    del studentsData[0]
         
